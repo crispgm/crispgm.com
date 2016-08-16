b0ad041189f12a06263be9802f8b644bffb4f453
David Zhang <crispgm@gmail.com>
2016-08-16 11:44:53 +0800

    Add mkdir of bin/src

--- opscript/build_system.sh
+++ opscript/build_system.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 useradd runtime --home /home/runtime --password 123
 mkdir -p /home/runtime
+mkdir -p /home/runtime/bin
+mkdir -p /home/runtime/src
 chown -R runtime /home/runtime
+
+echo "export  LC_ALL=en_US.UTF-8"  >> ~/.bash_profile
